7ec7a91027e30e20aaf329778dec3eb1a6a60482
Aarti Tanaji Bhosale <aartibh@cybage.com>
2025-11-26 20:05:44 +0530

    Adde sample file along with config file

---
+++ login.py
@@ -0,0 +1,10 @@
+# login.py
+import sqlite3
+
+def login(username, password):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+    cursor.execute(query)
+    user = cursor.fetchone()
+    return user
