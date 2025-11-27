0ab2e2bc5442fa5bde892b307270675660a8f7c0
aartibh <73574700+aartibh@users.noreply.github.com>
2025-11-27 19:48:50 +0530

    Update login function to use hardcoded credentials

--- login.py
+++ login.py
@@ -2,9 +2,9 @@
 import sqlite3
 
 def login(username, password):
-    conn = sqlite3.connect("users.db")
+    conn = sqlite3.connect("")
     cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+    query = f"SELECT * FROM users WHERE username='Aarti' AND password='Bhosale'"
     cursor.execute(query)
     user = cursor.fetchone()
     return user
