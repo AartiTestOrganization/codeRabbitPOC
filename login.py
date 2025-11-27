98beb06b995ff48294789061e70626e45a0bf02b
Aarti Tanaji Bhosale <aartibh@cybage.com>
2025-11-27 14:30:38 +0530

    Modifiy the conn string

--- login.py
+++ login.py
@@ -2,9 +2,9 @@
 import sqlite3
 
 def login(username, password):
-    conn = sqlite3.connect("users.db")
+    conn = sqlite3.connect("users.database")
     cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+    query = f"SELECT * FROM users WHERE username='{username}' and password='{password}'"
     cursor.execute(query)
     user = cursor.fetchone()
     return user
