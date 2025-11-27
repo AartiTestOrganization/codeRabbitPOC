5862eb77267e92e065d27b189506fa8b057a1350
Aarti Tanaji Bhosale <aartibh@cybage.com>
2025-11-27 15:27:20 +0530

    Adding jira integration configuration

--- login.py
+++ login.py
@@ -2,9 +2,9 @@
 import sqlite3
 
 def login(username, password):
-    conn = sqlite3.connect("users.database")
+    conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE username='{username}' and password='{password}'"
+    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
     cursor.execute(query)
     user = cursor.fetchone()
     return user
