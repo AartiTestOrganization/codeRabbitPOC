b1ee34f4dd44d760001bb10b8e8f2819ae12d636
aartibh <73574700+aartibh@users.noreply.github.com>
2025-11-27 19:57:21 +0530

    Change hardcoded login credentials in query

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ import sqlite3
 def login(username, password):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+    query = f"SELECT * FROM users WHERE username='Aarti' AND password='Bhosale'"
     cursor.execute(query)
     user = cursor.fetchone()
-    return user
+    return user;
